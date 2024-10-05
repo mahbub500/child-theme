d2ca56eecab75857eb30015b91a70306163dbe0b
Mahbub Rahman <38538845+mahbub500@users.noreply.github.com>
2024-10-05 20:34:49 +0600

    misc

--- blog.php
+++ blog.php
@@ -19,15 +19,8 @@ $query = new WP_Query([
 
 
 
-<!-- Begin Site Title
-================================================== -->
-<!-- <div class="container"> -->
-<!-- End Site Title
-================================================== -->
 
-    <!-- Begin Featured
-    ================================================== -->
-    <section class="featured-posts">
+<section class="featured-posts">
     <div class="section-title">
         <h2><span>Blog List</span></h2>
     </div>
@@ -55,8 +48,8 @@ $query = new WP_Query([
                 </div>
                 <div class="col-md-7">
                     <div class="card-block">
-                        <h2 class="card-title"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
-                        <h4 class="card-text"><?php echo get_the_content(); ?></h4>
+                        <h2 class="card-title mt-2"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
+                        <h4 class="card-text"><?php echo wp_trim_words(get_the_content(), 40, '...'); ?></h4>
                         <div class="metafooter">
                             <div class="wrapfooter">
                                 <span class="meta-footer-thumb">
@@ -66,7 +59,7 @@ $query = new WP_Query([
                                 <span class="post-name"><a href="<?php esc_url( $author_avatar ) ?>"><?php echo get_the_author(); ?></a></span><br/>
                                 <span class="post-date"><?php echo get_the_date('F j, Y') ?></span><span class="dot"></span><span class="post-read"><?php echo $reading_time; ?> min read</span>
                                 </span>
-                                <span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
+                                <span class="post-read-more"><a href="p<?php echo get_permalink(); ?>" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
                             </div>
                         </div>
                     </div>
@@ -78,8 +71,7 @@ $query = new WP_Query([
       <?php 
 
            }
-        
-        // Reset post data
+
         wp_reset_postdata();
     } else {
         echo 'No posts found';
@@ -92,9 +84,6 @@ $query = new WP_Query([
 
     </div>
     </section>
-    <!-- End Featured
-    ================================================== -->
-
 </div>
 
 
