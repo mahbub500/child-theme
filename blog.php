a9e3e9209d874f06fa14c5fb69639a507d4b0db7
Mahbub Rahman <38538845+mahbub500@users.noreply.github.com>
2024-10-06 06:54:23 +0600

    new style added

--- blog.php
+++ blog.php
@@ -18,73 +18,75 @@ $query = new WP_Query([
 
 
 
+<?php
+/*
+Template Name: Blog list
+*/
+
+get_header(); 
 
 
-<section class="featured-posts">
-    <div class="section-title">
-        <h2><span>Blog List</span></h2>
-    </div>
-    <div class="card-columns listfeaturedtag">
-
-     <?php 
-        if ( $query->have_posts() ) {
-        while ( $query->have_posts() ) {
-            $query->the_post(); 
-        $word_count = str_word_count( strip_tags( get_the_content() ) );
-        $reading_time = ceil( $word_count / 200 );
-
-        $author_email = get_the_author_meta('user_email'); 
-        $author_avatar = get_avatar_url( $author_email, ['size' => '96'] );
-     ?>
-
-        <!-- begin post -->
-        <div class="card">
-            <div class="row">
-                <div class="col-md-5 wrapthumbnail">
-                    <a href="<?php echo get_permalink(); ?>">
-                        <div class="thumbnail" style="background-image: url(<?php echo get_the_post_thumbnail_url( get_the_ID(), 'medium' ); ?>);">
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-7">
-                    <div class="card-block">
-                        <h2 class="card-title mt-2"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
-                        <h4 class="card-text"><?php echo wp_trim_words(get_the_content(), 40, '...'); ?></h4>
-                        <div class="metafooter">
-                            <div class="wrapfooter">
-                                <span class="meta-footer-thumb">
-                                <a href="<?php echo esc_url( $author_avatar ) ?>"><img class="author-thumb" src="<?php echo esc_url( $author_avatar ) ?>" alt="<?php echo get_the_author() ?>"></a>
-                                </span>
-                                <span class="author-meta">
-                                <span class="post-name"><a href="<?php esc_url( $author_avatar ) ?>"><?php echo get_the_author(); ?></a></span><br/>
-                                <span class="post-date"><?php echo get_the_date('F j, Y') ?></span><span class="dot"></span><span class="post-read"><?php echo $reading_time; ?> min read</span>
-                                </span>
-                                <span class="post-read-more"><a href="p<?php echo get_permalink(); ?>" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- end post -->
-
-      <?php 
-
-           }
-
-        wp_reset_postdata();
-    } else {
-        echo 'No posts found';
-    }
-
-       ?> 
+$query = new WP_Query([
+    'post_type'      => 'post',
+    'posts_per_page' => 5
+]);
 
 
+
+?>
+
+
+
+<div class="container">
+    <div class="row justify-content-md-center">
+         
       
+        <div class="col-10 pb-4 mb-4 " style="border-bottom: 1px solid #e7e7e7;" >
+            <article class="type-post entry has-post-thumbnail">
+            <header class="entry-header">
+                <h2 class="entry-title">
+                    <a class="entry-title-link custom-hover-effect" rel="bookmark" href="#" >
+                        How to Redesign a WordPress Website (Beginner’s Guide)
+                    </a>
+                </h2>
+                <p class="entry-meta">
+                    By <a href="https://www.wpbeginner.com/author/wpbeginner/" >Editorial Staff</a> |
+               
+                </p>
+            </header>
 
+            <div class="entry-summary">
+                <a class="entry-image-link" href="https://www.wpbeginner.com/beginners-guide/how-to-redesign-a-wordpress-website-beginners-guide/" aria-hidden="true" tabindex="-1">
+                    <img width="180" height="180" 
+                         src="https://www.wpbeginner.com/wp-content/uploads/2024/09/redesign-a-wordpress-website-thumbnail-180x180.png" 
+                         class="alignleft post-image entry-image entered lazyloaded" 
+                         alt="How to Redesign a WordPress Website" 
+                         decoding="async"
+                         srcset="" 
+                         sizes="(max-width: 180px) 100vw, 180px" />
+                </a>
+                <p>
+                    Is your WordPress site underperforming? Dropping traffic, low conversion rates, poor mobile experience, or slow loading times are clear signs it’s time for a redesign. But tackling a redesign can be daunting, as you might worry about losing SEO rankings. At WPBeginner, we’ve redesigned our…
+                    <strong>
+                        <a href="https://www.wpbeginner.com/beginners-guide/how-to-redesign-a-wordpress-website-beginners-guide/">Read More »</a>
+                    </strong>
+                </p>
+            </div>
+        </article>
+        </div>
+        
     </div>
-    </section>
+    
 </div>
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
 
 
 
---
+++ blog_.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Template Name: Blog list
+*/
+
+get_header(); 
+
+
+$query = new WP_Query([
+    'post_type'      => 'post',
+    'posts_per_page' => 5
+]);
+
+
+
+?>
+
+
+
+
+
+
+<section class="featured-posts">
+    <div class="section-title">
+        <h2><span>Blog List</span></h2>
+    </div>
+    <div class="card-columns listfeaturedtag">
+
+     <?php 
+        if ( $query->have_posts() ) {
+        while ( $query->have_posts() ) {
+            $query->the_post(); 
+        $word_count = str_word_count( strip_tags( get_the_content() ) );
+        $reading_time = ceil( $word_count / 200 );
+
+        $author_email = get_the_author_meta('user_email'); 
+        $author_avatar = get_avatar_url( $author_email, ['size' => '96'] );
+     ?>
+
+        <!-- begin post -->
+        <div class="card">
+            <div class="row">
+                <div class="col-md-5 wrapthumbnail">
+                    <a href="<?php echo get_permalink(); ?>">
+                        <div class="thumbnail" style="background-image: url(<?php echo get_the_post_thumbnail_url( get_the_ID(), 'medium' ); ?>);">
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-7">
+                    <div class="card-block">
+                        <h2 class="card-title mt-2"><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
+                        <h4 class="card-text"><?php echo wp_trim_words(get_the_content(), 40, '...'); ?></h4>
+                        <div class="metafooter">
+                            <div class="wrapfooter">
+                                <span class="meta-footer-thumb">
+                                <a href="<?php echo esc_url( $author_avatar ) ?>"><img class="author-thumb" src="<?php echo esc_url( $author_avatar ) ?>" alt="<?php echo get_the_author() ?>"></a>
+                                </span>
+                                <span class="author-meta">
+                                <span class="post-name"><a href="<?php esc_url( $author_avatar ) ?>"><?php echo get_the_author(); ?></a></span><br/>
+                                <span class="post-date"><?php echo get_the_date('F j, Y') ?></span><span class="dot"></span><span class="post-read"><?php echo $reading_time; ?> min read</span>
+                                </span>
+                                <span class="post-read-more"><a href="p<?php echo get_permalink(); ?>" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd"></path></svg></a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end post -->
+
+      <?php 
+
+           }
+
+        wp_reset_postdata();
+    } else {
+        echo 'No posts found';
+    }
+
+       ?> 
+
+
+      
+
+    </div>
+    </section>
+</div>
+
+
+
+<?php get_footer(); ?>
